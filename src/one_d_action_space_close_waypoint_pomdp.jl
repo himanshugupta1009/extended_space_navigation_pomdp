e787c5d3a7a09dcc73b309a0545c5a3e77c9c0e3
Himanshu Gupta <himanshu.gupta@colorado.edu>
2020-10-13 15:51:47 -0600

    Modify Gen function for 1D POMDP

--- src/one_d_action_space_close_waypoint_pomdp.jl
+++ src/one_d_action_space_close_waypoint_pomdp.jl
@@ -42,6 +42,8 @@ function is_terminal_state_pomdp_planning(s,terminal_state)
     end
 end
 
+
+
 #************************************************************************************************
 #Generate different scenarios which will represent the starting belief for the POMDP planner
 
@@ -62,6 +64,8 @@ function Base.rand(rng::AbstractRNG, state_distribution::POMDP_1D_action_space_s
     end
     return POMDP_state_1D_action_space(state_distribution.world.cart,pedestrians,state_distribution.start_path_index)
 end
+
+
 
 #************************************************************************************************
 #Simulating Human One step forward in POMDP planning
@@ -145,11 +149,11 @@ end
 #@code_warntype update_human_position_pomdp_planning(env.humans[2], env, 1.0, MersenneTwister(1234))
 
 
+
 #************************************************************************************************
 #Simulating the cart one step forward in POMDP planning according to its new speed
 
 function update_cart_position_pomdp_planning(current_cart_position, new_cart_velocity, starting_index, world)
-    arc_length = new_cart_velocity
     time_interval = 1.0/new_cart_velocity
     current_x, current_y, current_theta = current_cart_position.x, current_cart_position.y, current_cart_position.theta
     length_hybrid_a_star_path = length(world.cart_hybrid_astar_path)
@@ -162,16 +166,14 @@ function update_cart_position_pomdp_planning(current_cart_position, new_cart_vel
             steering_angle = world.cart_hybrid_astar_path[starting_index]
             if(steering_angle == 0.0)
                 new_theta = current_theta
-                new_x = current_x + arc_length*cos(current_theta)*time_interval
-                new_y = current_y + arc_length*sin(current_theta)*time_interval
+                new_x = current_x + new_cart_velocity*cos(current_theta)*time_interval
+                new_y = current_y + new_cart_velocity*sin(current_theta)*time_interval
             else
-                new_theta = current_theta + (arc_length * tan(steering_angle) * time_interval / world.cart.L)
+                new_theta = current_theta + (new_cart_velocity * tan(steering_angle) * time_interval / world.cart.L)
                 new_theta = wrap_between_0_and_2Pi(new_theta)
                 new_x = current_x + ((world.cart.L / tan(steering_angle)) * (sin(new_theta) - sin(current_theta)))
                 new_y = current_y + ((world.cart.L / tan(steering_angle)) * (cos(current_theta) - cos(new_theta)))
             end
-            new_x = clamp(new_x,0,world.length)
-            new_y = clamp(new_y,0,world.breadth)
             push!(cart_path,(Float64(new_x), Float64(new_y), Float64(new_theta)))
             current_x, current_y,current_theta = new_x,new_y,new_theta
             starting_index = starting_index + 1
@@ -184,6 +186,7 @@ function update_cart_position_pomdp_planning(current_cart_position, new_cart_vel
 end
 # @code_warntype update_cart_position_pomdp_planning(env.cart, 0.0, 1, env)
 # update_cart_position_pomdp_planning(env.cart, 0, 1, env)
+
 
 
 #************************************************************************************************
@@ -200,30 +203,27 @@ end
 
 # Goal Reward
 function goal_reward_pomdp_planning(s, distance_threshold, goal_reached_flag, goal_reward)
-    total_reward = 0.0
     if(goal_reached_flag)
-        total_reward = goal_reward
+        return goal_reward
     else
         euclidean_distance = ((s.cart.x - s.cart.goal.x)^2 + (s.cart.y - s.cart.goal.y)^2)^0.5
         if(euclidean_distance<distance_threshold && euclidean_distance!=0.0)
-            total_reward = goal_reward/euclidean_distance
+            return goal_reward/euclidean_distance
+        else
+            return 0.0
         end
     end
-    #@show(total_reward)
-    return total_reward
 end
 
 # Low Speed Penalty
 function speed_reward_pomdp_planning(s, max_speed)
-    t = (s.cart.v - max_speed)/max_speed
+    t::Float64 = (s.cart.v - max_speed)/max_speed
     return t
 end
 
 # Penalty for excessive acceleration and deceleration
 function unsmooth_motion_penalty_pomdp_planning(s,a,sp)
-    if(sp.cart.x==-100.0 && sp.cart.y==-100.0)
-        return 0.0
-    elseif(a==1.0 || a==-1.0)
+    if(a==1.0 || a==-1.0)
         return -1.0
     else
         return 0.0
@@ -237,6 +237,9 @@ function immediate_stop_penalty_pomdp_planning(immediate_stop_flag, penalty)
         return 0.0
     end
 end
+
+
+
 #************************************************************************************************
 #POMDP Generative Model
 
@@ -259,20 +262,17 @@ function POMDPs.gen(m::POMDP_Planner_1D_action_space, s, a, rng)
         new_cart_position = (-100.0, -100.0, -100.0)
         cart_reached_goal_flag = true
         new_cart_velocity = clamp(s.cart.v + a, 0.0, m.max_cart_speed)
-    # elseif(a == -10.0)
-    #     new_cart_position = (-100.0, -100.0, -100.0)
-    #     immediate_stop_flag = true
-    #     new_cart_velocity = clamp(s.cart.v + a, 0.0, m.max_cart_speed)
+    elseif(a == -10.0)
+        new_cart_position = (-100.0, -100.0, -100.0)
+        immediate_stop_flag = true
+        new_cart_velocity = clamp(s.cart.v + a, 0.0, m.max_cart_speed)
     else
-        if(a == -10.0)
-            immediate_stop_flag = true
-        end
         new_cart_velocity =clamp(s.cart.v + a, 0.0, m.max_cart_speed)
         cart_path::Vector{Tuple{Float64,Float64,Float64}} = update_cart_position_pomdp_planning(s.cart, new_cart_velocity, s.current_path_covered_index, m.world)
         new_cart_position = cart_path[end]
-        for positions in cart_path
+        for position in cart_path
             for human in s.pedestrians
-                if(is_within_range(location(s.cart.x,s.cart.y),location(human.x,human.y),m.pedestrian_distance_threshold))
+                if( is_within_range_check_with_points(position[1],position[2], human.x, human.y, m.pedestrian_distance_threshold) )
                     #@show("Collision happened",s)
                     new_cart_position = (-100.0, -100.0, -100.0)
                     collision_with_pedestrian_flag = true
@@ -280,31 +280,38 @@ function POMDPs.gen(m::POMDP_Planner_1D_action_space, s, a, rng)
                 end
             end
         end
-        # Simulate all the pedestrians
-        for human in s.pedestrians
-            modified_human_state,observed_location = update_human_position_pomdp_planning(human, m.world, one_time_step, rng)
-            push!(new_human_states, modified_human_state)
-            push!(observed_positions, observed_location)
-        end
-
+    end
+    # Simulate all the pedestrians
+    for human in s.pedestrians
+        modified_human_state,observed_location = update_human_position_pomdp_planning(human, m.world, one_time_step, rng)
+        push!(new_human_states, modified_human_state)
+        push!(observed_positions, observed_location)
     end
     cart_new_state = cart_state(new_cart_position[1], new_cart_position[2], new_cart_position[3], new_cart_velocity, s.cart.L, s.cart.goal)
 
     # Generate starting index in Hybrid A* path for the next POMPD state
     new_path_index = s.current_path_covered_index + new_cart_velocity
-    #@show(s.current_path_covered_index,new_cart_velocity,new_path_index)
-    #@show(cart_reached_goal_flag,collision_with_pedestrian_flag)
 
     # Next POMDP State
     sp = POMDP_state_1D_action_space(cart_new_state, new_human_states, new_path_index)
     # Generated Observation
     o = observed_positions
+
     # R(s,a): Reward for being at state s and taking action a
+    #Penalize if collision
     r = collision_penalty_pomdp_planning(collision_with_pedestrian_flag, m.pedestrian_collision_penalty)
-        + goal_reward_pomdp_planning(s, m.goal_reward_distance_threshold, cart_reached_goal_flag, m.goal_reward)
-        + speed_reward_pomdp_planning(s, m.max_cart_speed)
-        + unsmooth_motion_penalty_pomdp_planning(s,a,sp)
-    #    immediate_stop_penalty_pomdp_planning(immediate_stop_flag, m.pedestrian_collision_penalty)
+    #println("Reward from collision ", r)
+    #Reward if reached goal
+    r += goal_reward_pomdp_planning(s, m.goal_reward_distance_threshold, cart_reached_goal_flag, m.goal_reward)
+    #println("Reward from goal ", r)
+    #Penalize if going slow when it can go fast
+    r += speed_reward_pomdp_planning(s, m.max_cart_speed)
+    #println("Reward from max_speed ", r)
+    #Penalize if had to apply sudden brakes
+    r += immediate_stop_penalty_pomdp_planning(immediate_stop_flag, m.pedestrian_collision_penalty)
+    #println("Reward from immediate stoping ", r)
+    #To penalize unsmooth paths
+    #r += unsmooth_motion_penalty_pomdp_planning(s,a,sp) +
     #Small Penalty for longer duration paths
     r = r - 1
     return (sp=sp, o=o, r=r)
@@ -312,44 +319,50 @@ end
 #@code_warntype POMDPs.gen(golfcart_pomdp(), SP_POMDP_state(env.cart,env.humans,1), 1, MersenneTwister(1234))
 
 
+
 #************************************************************************************************
 #Upper bound and lower bound values for DESPOT
 
-function is_collision_state_pomdp_planning(s,m)
+function is_collision_state_pomdp_planning_1D_action_space(s,m)
     for human in s.pedestrians
-        if(is_within_range(location(s.cart.x,s.cart.y),location(human.x,human.y),m.pedestrian_distance_threshold))
+        if( is_within_range_check_with_points(s.cart.x,s.cart.y, human.x, human.y, m.pedestrian_distance_threshold) )
             return true
         end
     end
     return false
 end
 
-function time_to_goal_pomdp_planning(s,m)
-    remaining_path_length = length(m.world.cart_hybrid_astar_path) - s.current_path_covered_index
+function time_to_goal_pomdp_planning_1D_action_space(s,m)
+    if(length(m.world.cart_hybrid_astar_path) == 0)
+        remaining_path_length = sqrt( (s.cart.x-s.cart.goal.x)^2 + (s.cart.y-s.cart.goal.y)^2 )
+    else
+        remaining_path_length = length(m.world.cart_hybrid_astar_path) - s.current_path_covered_index
+    end
     return floor(remaining_path_length/m.max_cart_speed)
 end
 
-function calculate_upper_bound_value_pomdp_planning(m, b)
+function calculate_upper_bound_value_pomdp_planning_1D_action_space(m::POMDP_Planner_1D_action_space, b)
     value_sum = 0.0
     for (s, w) in weighted_particles(b)
-        if(is_within_range(location(s.cart.x,s.cart.y), s.cart.goal, m.cart_goal_reached_distance_threshold))
+        if (s.cart.x == -100.0 && s.cart.y == -100.0)
+            value_sum += 0.0
+        elseif (is_within_range_check_with_points(s.cart.x, s.cart.y, s.cart.goal.x, s.cart.goal.y, m.cart_goal_reached_distance_threshold))
             value_sum += w*m.goal_reward
-        elseif (is_collision_state_pomdp_planning(s,m))
+        elseif (is_collision_state_pomdp_planning_1D_action_space(s,m))
             value_sum += w*m.pedestrian_collision_penalty
         else
-            value_sum += w*((discount(m)^time_to_goal_pomdp_planning(s,m))*m.goal_reward)
+            value_sum += w*((discount(m)^time_to_goal_pomdp_planning_1D_action_space(s,m))*m.goal_reward)
         end
     end
     return value_sum
 end
 #@code_warntype calculate_upper_bound_value(golfcart_pomdp(), initialstate_distribution(golfcart_pomdp()))
 
-function calculate_lower_bound_policy_pomdp_planning(b)
+function calculate_lower_bound_policy_pomdp_planning_1D_action_space(b)
     #Implement a reactive controller for your lower bound
-    action_to_be_returned = 0.0
-    chosen_action = 0.0
-    d_far_threshold = 5.0
-    d_near_threshold =  2.0
+    action_to_be_returned = 1.0
+    d_far_threshold = 6.0
+    d_near_threshold = 3.0
     for (s, w) in weighted_particles(b)
         dist_to_closest_human = 200.0  #Some big number (not Inf) that is not feasible
         for human in s.pedestrians
@@ -357,20 +370,59 @@ function calculate_lower_bound_policy_pomdp_planning(b)
             if(euclidean_distance < dist_to_closest_human)
                 dist_to_closest_human = euclidean_distance
             end
+            if(dist_to_closest_human<d_near_threshold)
+                return -1.0
+            end
         end
         if(dist_to_closest_human>d_far_threshold)
             chosen_action = 1.0
-        elseif (dist_to_closest_human < d_near_threshold)
-            chosen_action = -1.0
         else
             chosen_action = 0.0
         end
         if(chosen_action < action_to_be_returned)
             action_to_be_returned = chosen_action
-            if(action_to_be_returned == -1.0)
-                return action_to_be_returned
-            end
         end
     end
     return action_to_be_returned
+end
+
+
+
+#************************************************************************************************
+#Functions for debugging lb>ub error
+
+function debug_is_collision_state_pomdp_planning_1D_action_space(s,m)
+    for human in s.pedestrians
+        if(is_within_range(location(s.cart.x,s.cart.y),location(human.x,human.y),m.pedestrian_distance_threshold))
+            println("Collision with human ", human)
+            return true
+        end
+    end
+    return false
+end
+
+function debug_golfcart_upper_bound_1D_action_space(m,b)
+
+    lower = lbound(DefaultPolicyLB(FunctionPolicy(calculate_lower_bound_policy_pomdp_planning_1D_action_space), max_depth=50),m , b)
+
+    value_sum = 0.0
+    for (s, w) in weighted_particles(b)
+        if (s.cart.x == -100.0 && s.cart.y == -100.0)
+            value_sum += 0.0
+        elseif (is_within_range(location(s.cart.x,s.cart.y), s.cart.goal, m.cart_goal_reached_distance_threshold))
+            value_sum += w*m.goal_reward
+        elseif (is_collision_state_pomdp_planning_1D_action_space(s,m))
+            value_sum += w*m.pedestrian_collision_penalty
+        else
+            value_sum += w*((discount(m)^time_to_goal_pomdp_planning_1D_action_space(s,m))*m.goal_reward)
+        end
+    end
+    #@show("*********************************************************************")
+    #@show(value_sum)
+    u = (value_sum)/weight_sum(b)
+    if lower > u
+        push!(bad, (lower,u,b))
+        @show("IN DEBUG",lower,u)
+    end
+    return u
 end
