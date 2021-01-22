697f5a8c4ca87a8dacf0ce3f1c03f3e5f09d407d
himanshugupta1009 <himanshu.gupta@colorado.edu>
2021-01-22 10:45:40 -0700

    Add small changes to parameter values

--- src/new_main_2d_action_space_pomdp.jl
+++ src/new_main_2d_action_space_pomdp.jl
@@ -122,7 +122,7 @@ function run_one_simulation(env_right_now, user_defined_rng, m, planner)
     number_risks = 0
     one_time_step = 0.5
     lidar_range = 30
-    num_humans_to_care_about_while_pomdp_planning = 10
+    num_humans_to_care_about_while_pomdp_planning = 6
     #cone_half_angle = (pi)/3.0
     cone_half_angle = (2.0/3.0)*pi
     number_of_sudden_stops = 0
@@ -221,6 +221,8 @@ function run_one_simulation(env_right_now, user_defined_rng, m, planner)
                                                                     cone_half_angle, lidar_range,MersenneTwister( Int64( floor( 100*rand(user_defined_rng) ) ) ) )
                 current_belief =  get_belief_for_selected_humans_from_belief_over_complete_lidar_data(current_belief_over_complete_cart_lidar_data,
                                                                     env_right_now.complete_cart_lidar_data, env_right_now.cart_lidar_data)
+
+
                 number_risks += risks_in_simulation
             end
 
@@ -273,18 +275,18 @@ run_simulation_flag = true
 if(run_simulation_flag)
 
     # #env = generate_environment_no_obstacle(MersenneTwister(71))
-    env = generate_environment_circular_obstacles(300, MersenneTwister(15))
+    env = generate_environment_no_obstacles(300, MersenneTwister(15))
     env_right_now = deepcopy(env)
 
     #Create POMDP for env_right_now
-    golfcart_2D_action_space_pomdp = POMDP_Planner_2D_action_space(0.99,2.0,-100.0,1.0,-100.0,1.0,1.0,100.0,5.0,env_right_now,1)
+    golfcart_2D_action_space_pomdp = POMDP_Planner_2D_action_space(0.99,2.0,-100.0,1.0,-100.0,1.0,1.0,100.0,5.0,env_right_now)
     discount(p::POMDP_Planner_2D_action_space) = p.discount_factor
     isterminal(::POMDP_Planner_2D_action_space, s::POMDP_state_2D_action_space) = is_terminal_state_pomdp_planning(s,location(-100.0,-100.0));
     actions(::POMDP_Planner_2D_action_space) = [(-10.0,-10.0),(-pi/4,0.0),(-pi/6,0.0),(-pi/12,0.0),(0.0,-1.0),(0.0,0.0),(0.0,1.0),(pi/12,0.0),(pi/6,0.0),(pi/4,0.0)]
 
     solver = DESPOTSolver(bounds=IndependentBounds(DefaultPolicyLB(FunctionPolicy(calculate_lower_bound_policy_pomdp_planning_2D_action_space),max_depth=100,
                             final_value=reward_to_be_awarded_at_max_depth_in_lower_bound_policy_rollout),
-                            calculate_upper_bound_value_pomdp_planning_2D_action_space, check_terminal=true),K=100,D=100,T_max=0.5, tree_in_info=true)
+                            calculate_upper_bound_value_pomdp_planning_2D_action_space, check_terminal=true),K=100,D=100,T_max=0.5, tree_in_info=true, default_action=(-10.0,-10.0))
     planner = POMDPs.solve(solver, golfcart_2D_action_space_pomdp);
 
     display_env(golfcart_2D_action_space_pomdp.world)
