f7311b68fada280b1f17ef0769060f2164a2be0a
Himanshu Gupta <himanshu@omak.colorado.edu>
2021-06-14 11:31:13 -0600

    Modify pedestrian distance threshold for 2D and 1D action space POMDP

--- src/run_experiments_scenario1.jl
+++ src/run_experiments_scenario1.jl
@@ -10,7 +10,7 @@ function run_experiment_for_given_world_and_noise_with_2D_POMDP_planner(world, r
     #Create POMDP for env_right_now
     env_right_now = deepcopy(world)
 
-    golfcart_2D_action_space_pomdp = POMDP_Planner_2D_action_space(0.97,0.5,-100.0,1.0,-100.0,0.0,1.0,1000.0,5.0,env_right_now)
+    golfcart_2D_action_space_pomdp = POMDP_Planner_2D_action_space(0.97,1.0,-100.0,1.0,-100.0,0.0,1.0,1000.0,5.0,env_right_now)
 
     solver = DESPOTSolver(bounds=IndependentBounds(DefaultPolicyLB(FunctionPolicy(calculate_lower_bound_policy_pomdp_planning_2D_action_space),max_depth=100),
                             calculate_upper_bound_value_pomdp_planning_2D_action_space, check_terminal=true),K=50,D=100,T_max=0.5, tree_in_info=true)
@@ -43,7 +43,7 @@ function run_experiment_for_given_world_and_noise_with_1D_POMDP_planner(world, r
     #Create POMDP for env_right_now
     env_right_now = deepcopy(world)
 
-    golfcart_1D_action_space_pomdp = POMDP_Planner_1D_action_space(0.97,0.5,-100.0,1.0,1.0,1000.0,5.0,env_right_now,1)
+    golfcart_1D_action_space_pomdp = POMDP_Planner_1D_action_space(0.97,1.0,-100.0,1.0,1.0,1000.0,5.0,env_right_now,1)
 
     #actions(::POMDP_Planner_1D_action_space) = Float64[-0.5, 0.0, 0.5, -10.0]
 
@@ -80,6 +80,7 @@ function run_experiment_pipeline(num_humans, num_simulations)
     rand_rng = MersenneTwister(100)
 
     for iteration_num in 1:num_simulations
+	println("\n Running Simulation #", string(iteration_num), "\n")
         rand_noise_generator_seed = Int(ceil(100*rand(rand_rng)))
         rand_noise_generator = MersenneTwister(rand_noise_generator_seed)
         experiment_env = generate_environment_no_obstacles(num_humans, rand_noise_generator)
@@ -124,7 +125,7 @@ function run_experiment_pipeline(num_humans, num_simulations)
 end
 
 average_time_taken_2D_POMDP_planner, total_safe_paths_2D_POMDP_planner, average_sudden_stops_2D_POMDP_planner,
-average_time_taken_1D_POMDP_planner, total_safe_paths_1D_POMDP_planner, average_sudden_stops_1D_POMDP_planner = run_experiment_pipeline(300,100)
+average_time_taken_1D_POMDP_planner, total_safe_paths_1D_POMDP_planner, average_sudden_stops_1D_POMDP_planner = run_experiment_pipeline(200,100)
 
 #=
 average_time_taken_2D_POMDP_planner, total_safe_paths_2D_POMDP_planner, average_sudden_stops_2D_POMDP_planner,
