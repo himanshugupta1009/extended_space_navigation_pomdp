e4ef30bfd4589a5bcf593bb272241378c78e11a9
himanshugupta1009 <himanshu.gupta@colorado.edu>
2021-09-02 20:34:35 -0600

    Modify cart_lidar_data to complete_cart_lidar_data for shielding

--- src/utils.jl
+++ src/utils.jl
@@ -569,7 +569,7 @@ function find_distance_between_two_points(p1_x,p1_y, p2_x, p2_y)
 end
 
 function is_there_immediate_collision_with_pedestrians(world, pedestrian_distance_threshold)
-    for human in world.cart_lidar_data
+    for human in world.complete_cart_lidar_data
         if( find_if_two_circles_intersect(world.cart.x, world.cart.y, world.cart.L,human.x, human.y, pedestrian_distance_threshold) )
             return true
         end
