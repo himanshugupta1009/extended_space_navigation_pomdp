d50740db4bef0ef137faa70e9d6c13efd36eadbc
himanshugupta1009 <himanshu.gupta@colorado.edu>
2021-09-09 07:28:15 -0600

    Modify gif and txt files

---
+++ src/run_pipeline.sh
@@ -0,0 +1,3 @@
+/home/julia-1.6.2/bin/julia run_experiments_scenario1_combined.jl 400 10
+/home/julia-1.6.2/bin/julia run_experiments_scenario2_combined.jl 400 10
+/home/julia-1.6.2/bin/julia run_experiments_scenario4_combined.jl 400 10
